0dc8a9e92d98dfebddc265688310bf2e09776a8b
w3aran <w3aran@users.noreply.github.com>
2015-11-24 19:09:44 -0500

    Update install_ansible_on_ubuntu_14.sh

--- install_ansible_on_ubuntu_14.sh
+++ install_ansible_on_ubuntu_14.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install ansible -y
